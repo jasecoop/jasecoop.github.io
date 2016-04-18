31a4432b8611e68653f2023bb60623aeff099743
jasecoop <jase@jasecoop.com>
2016-04-18 16:47:02 +0800

    session

--- javascript/pw.js
+++ javascript/pw.js
@@ -1,9 +1,19 @@
 $(document).ready( function(){
+  var access = window.localStorage.getItem("access");
+
+  if ( $('body').hasClass('session') && access != "y") {
+    window.location.href = "/info";
+  }
+
+  if ( $('body').hasClass('info') && access === "y") {
+    window.location.href = "/0000";
+  }
 
   $('form').on('submit', function(e) {
     e.preventDefault();
     var pw = $('#pw').val();
     if ( pw == 'rhythmicrituals') {
+      window.localStorage.setItem("access", "y");
       window.location.href = "/0000";
     } else {
       var html = '<div class="header-item error-msg"><span>Password incorrect</span>'
